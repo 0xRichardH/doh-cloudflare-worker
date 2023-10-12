3df9476ca827e7b46d100a3c1d93a19670d4fc96
Richard Hao <richard@0xdev.dev>
2023-10-12 21:27:55 +0800

    feat: forward requests to doh resolver

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
-use std::{cmp, collections::HashMap};
+use std::collections::HashMap;
 
 use worker::*;
+
+pub use console_error_panic_hook::set_once as set_panic_hook;
 
 const DOH: &str = "https://security.cloudflare-dns.com/dns-query";
 const DOH_JSON: &str = "https://security.cloudflare-dns.com/dns-query";
@@ -8,11 +10,48 @@ const CONTENT_TYPE: &str = "application/dns-message";
 const ACCEPT_TYPE: &str = "application/dns-json";
 
 #[event(fetch)]
-async fn main(req: Request, _env: Env, _ctx: Context) -> Result<Response> {
+async fn main(mut req: Request, _env: Env, _ctx: Context) -> Result<Response> {
+    set_panic_hook();
+
     match req.method() {
-        Method::Post if has_dns_content_type(req.headers()) => {}
-        Method::Get if has_dns_accept_type(req.headers()) => {}
-        Method::Get if has_dns_params(req.url()) => {}
+        Method::Post if has_dns_content_type(req.headers()) => {
+            let body = req.bytes().await?;
+            reqwest::Client::new()
+                .post(DOH)
+                .header("Accept", CONTENT_TYPE)
+                .header("Content-Type", CONTENT_TYPE)
+                .body(body)
+                .send()
+                .await
+                .map_err(map_reqwest_http_error)?;
+        }
+
+        Method::Get if has_dns_accept_type(req.headers()) => {
+            if let Ok(url) = req.url() {
+                let mut doh_json_url = Url::parse(DOH_JSON)?;
+                doh_json_url.set_query(url.query());
+                reqwest::Client::new()
+                    .get(doh_json_url)
+                    .header("Accept", ACCEPT_TYPE)
+                    .send()
+                    .await
+                    .map_err(map_reqwest_http_error)?;
+            }
+        }
+
+        Method::Get if has_dns_params(req.url()) => {
+            if let Ok(url) = req.url() {
+                let mut doh_url = Url::parse(DOH)?;
+                doh_url.set_query(url.query());
+                reqwest::Client::new()
+                    .get(doh_url)
+                    .header("Accept", CONTENT_TYPE)
+                    .send()
+                    .await
+                    .map_err(map_reqwest_http_error)?;
+            }
+        }
+
         _ => {}
     }
 
@@ -42,4 +81,8 @@ fn has_dns_params(url_result: Result<Url>) -> bool {
     }
 
     false
+}
+
+fn map_reqwest_http_error(error: reqwest::Error) -> worker::Error {
+    worker::Error::RustError(format!("reqwest::Error: {:?}", error))
 }
